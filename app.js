5af8b98f114bbad67451612f8cee82165d6e4204
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-07-26 18:48:14 +0100

    Removed unnecessary fi;es and displayed beginning of timer

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const timer = document.getElementById("timer");
 
+window.addEventListener("load", () => {
+  timer.innerText = `${times.minutes} ${times.seconds}`;
+});
+
+const times = {
+  minutes: 25,
+  seconds: "0" + "0",
+};
