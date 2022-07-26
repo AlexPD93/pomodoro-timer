ed0f15ce4ed30822c497c656238cc2709486d4c1
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-07-26 20:03:07 +0100

    Started decrease seconds function

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
 const timer = document.getElementById("timer");
+const startButton = document.getElementById("startButton");
 
-window.addEventListener("load", () => {
-  timer.innerText = `${times.minutes} ${times.seconds}`;
+let stopTime = true;
+let timeout;
+
+let [minutes, seconds] = [25, 0];
+
+function startTimer() {
+  if (startButton.innerText === "Start") {
+    stopTime = false;
+    startButton.innerText = "Stop";
+    decreaseSeconds();
+  } else if (startButton.innerText === "Stop") {
+    startButton.innerText = "Start";
+  }
+}
+
+function decreaseSeconds() {
+  seconds = parseInt(seconds);
+  if (stopTime === false) {
+    if (seconds < 10 || seconds === 0) {
+      seconds = "0" + seconds;
+    }
+
+    if (seconds === "0" + 0) {
+      setTimeout("seconds = 59", 1000);
+    }
+    if (seconds === 59) {
+      seconds = seconds - 1;
+    }
+    seconds = seconds - 1;
+
+    timer.innerHTML = `${minutes}:${seconds}`;
+    timeout = setTimeout("decreaseSeconds()", 1000);
+  }
+}
+
+function decreaseMinutes() {}
+
+// Event listeners
+
+document.addEventListener("click", () => {
+  startTimer();
 });
-
-const times = {
-  minutes: 25,
-  seconds: "0" + "0",
-};
